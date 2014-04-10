9001b8eaabc8f1aa92ea27035384752bc78d0d99
Nate Young <nate.young@code42.com>
2014-04-10 15:27:38 -0500

    trivial change to mark when I'm taking a break on this

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject bank_ocr_kata "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Bank OCR Kata"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
